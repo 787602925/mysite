02f050591a46c87127771e0e7b3d2b7f1c4968c3
Kehao <787602925@qq.com>
2025-09-30 11:49:17 +0200

    feat: construct structure; design card layout of homepage

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    cards = [
+        {"title": "简历", "url": "/resume/", "desc": "我的在线简历"},
+        {"title": "秋招", "url": "/blog/autumn/", "desc": "秋招相关的记录"},
+        {"title": "日程", "url": "/schedule/", "desc": "日程安排"}
+    ]
+    return render(request, "home/index.html", {"cards": cards})
